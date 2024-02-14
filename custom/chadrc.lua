bab74f5cdde6cc9c7f8970d680f0e4cf84fff085
sephclt <josephajero22@gmail.com>
2024-02-14 22:00:39 +0800

    initial commit

---
+++ custom/chadrc.lua
@@ -0,0 +1,15 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+  },
+
+  theme = "catppuccin",
+}
+
+M.plugins = "custom.plugins"
+M.mappings = require("custom.mappings")
+
+return M
---
+++ custom/plugins.lua
@@ -0,0 +1,95 @@
+local plugins = {
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    build = ":Neorg sync-parsers",
+    -- tag = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Documents/Notes/Neorg/Notes/",
+                school = "~/Documents/Notes/Neorg/School/",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function ()
+      require "custom.configs.copilot"
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+      }
+    }
+  }
+}
+
+return plugins
