51f6cf857b45f42afe47b5803871a78656c17d46
sephclt <josephajero22@gmail.com>
2024-02-15 09:12:48 +0800

    backup

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -7,6 +7,10 @@ M.ui = {
   },
 
   theme = "catppuccin",
+
+  nvdash = {
+    load_on_startup = true
+  }
 }
 
 M.plugins = "custom.plugins"
--- custom/plugins.lua
+++ custom/plugins.lua
@@ -10,6 +10,11 @@ local plugins = {
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          },
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
               workspaces = {
